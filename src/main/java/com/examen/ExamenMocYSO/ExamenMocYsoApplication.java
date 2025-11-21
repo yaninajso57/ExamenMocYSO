25622866a18b5752c29f4c56802867bc187f38c6
yaninajso57 <yaninajso57@educastur.es>
2025-11-21 17:31:39 +0100

    Initial commit

---
+++ src/main/java/com/examen/ExamenMocYSO/ExamenMocYsoApplication.java
@@ -0,0 +1,13 @@
+package com.examen.ExamenMocYSO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocYsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocYsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/ExamenMocYSO/ExamenMocYsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.ExamenMocYSO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocYsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
